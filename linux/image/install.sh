ba220be4c2159175200546a08ce51c2246dd73c4
Billy.Zheng <zw963@163.com>
2018-01-21 15:59:42 +0800

    Use aliyun centos source.

--- linux/image/install.sh
+++ linux/image/install.sh
@@ -25,7 +25,12 @@ function create_group()
 set -x
 
 cd /tmp
-rpm -Uvh http://mirror.overthewire.com.au/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+# rpm -Uvh http://mirror.overthewire.com.au/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+curl -o /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo
+
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
+
 yum update -y
 yum install -y --skip-broken --enablerepo centosplus \
 	centos-release-SCL file db4-utils compat-db43 mock wget s3cmd
